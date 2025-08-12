3e604da22cd1bee2ca23e7ae7f8cf967b849c3c9
wonzz <unbi53@gmail.com>
2025-08-12 15:02:12 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 172 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    // 최대공약수 (유클리드)
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+    
+    // 최소공배수
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(gcd(a, b));
+        System.out.println(lcm(a, b));
+    }
+}
